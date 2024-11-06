3dbf1a0bf28c04b42f459d69c49e233c56df38a4
KrozhDev <roko.mdn@gmail.com>
2024-11-06 15:54:56 +0300

    first commit

---
+++ src/test/java/com/project/movies_manager/MoviesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.movies_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
